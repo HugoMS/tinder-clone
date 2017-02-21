2029029a8833d19fb523c3db38fe9fa95e935f9b
hugomorelli <hugomorelli2@gmail.com>
2017-02-20 21:05:41 -0400

    sign up and Log in

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -12,6 +12,90 @@ import Parse
 
 class ViewController: UIViewController {
 
+    var signupMode = true
+    //MARK: Properties
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var signupOrLoginButton: UIButton!
+    @IBOutlet weak var changeSignupModeButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    //MARK: Actions
+    
+    @IBAction func signupOrLogin(_ sender: AnyObject) {
+        
+        let user = PFUser()
+        if signupMode {
+            user.username = usernameField.text
+            user.password = passwordField.text
+            
+            user.signUpInBackground { (success, error) in
+                
+                if error != nil {
+                    
+                    var errorMessage = "Sign Up Failed - please try again"
+                    
+                    if let parseError  = (error! as NSError).userInfo["error"] as? String {
+                        
+                        errorMessage = parseError
+                    }
+                    
+                    self.errorLabel.text = errorMessage
+                } else{
+                    
+                    print("Sign Up")
+                    
+                }
+            }
+        }
+        else{
+            
+            PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!, block: { (user, error) in
+                
+                if error != nil {
+                    
+                    var errorMessage = "Sign Up Failed - please try again"
+                    
+                    if let parseError  = (error! as NSError).userInfo["error"] as? String {
+                        
+                        errorMessage = parseError
+                    }
+                    
+                    self.errorLabel.text = errorMessage
+                } else{
+                    
+                    print("Logged In")
+                    
+                }
+            })
+                
+            
+        }
+        
+    }
+    
+    @IBAction func changeSignup(_ sender: AnyObject) {
+        
+        if signupMode{
+            
+            signupMode = false
+            
+            signupOrLoginButton.setTitle("Log In", for: [])
+            
+            changeSignupModeButton.setTitle("Sing Up", for: [])
+            
+        } else {
+            
+            signupMode = true
+
+            
+            signupOrLoginButton.setTitle("Sign Up", for: [])
+            
+            changeSignupModeButton.setTitle("Log In ", for: [])
+            
+        }
+        
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
